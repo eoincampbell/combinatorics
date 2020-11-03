0c3b34625d7c0f4e10d8e71b9872cb94d0a7ff0e
Stephen Cleary <github.ourteddybear@xoxy.net>
2020-11-02 22:50:32 -0500

    Use Math.DivRem.

--- src/Nito.Combinatorics/SmallPrimeUtility.cs
+++ src/Nito.Combinatorics/SmallPrimeUtility.cs
@@ -31,10 +31,17 @@ namespace Nito.Combinatorics
 
             while (i > 1)
             {
-                if (i % prime == 0)
+#if NETSTANDARD1_1
+                var remainder = i % prime;
+                var divResult = remainder == 0 ? i / prime : 0;
+#else
+                var divResult = Math.DivRem(i, prime, out var remainder);
+#endif
+
+                if (remainder == 0)
                 {
                     factors.Add(prime);
-                    i /= prime;
+                    i = divResult;
                 }
                 else
                 {
